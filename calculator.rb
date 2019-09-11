75dd02f7c7568d8f3ccf9bc6db625772fd2d4760
Adam chernitsky <adam987@comcast.net>
2019-09-11 18:20:10 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,35 @@
 # Add your variables here
+first_number = 0
+
+second_number = 1
+
+sum = first_number + second_number
+def add(sum)
+  sum
+end
+
+add(sum)
+
+
+difference = first_number - second_number
+def subtract(difference)
+  difference
+end
+
+subtract(difference)
+
+
+product = first_number * second_number
+def multiplying(product)
+  product
+end
+
+multiplying(product)
+
+
+quotient = first_number / second_number
+def dividing(quotient)
+  quotient
+end
+
+dividing(quotient)
