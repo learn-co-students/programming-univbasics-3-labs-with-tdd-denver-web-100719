dd8d164f8753d7f155976fbe699e271de0b4bc06
Seth Maisel <seth.maisel@gmail.com>
2019-09-28 23:30:56 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,13 @@
-# Add your variables here
+first_number = 5
+second_number = 12
+sum = first_number + second_number;
+puts sum;
+
+difference = first_number - second_number;
+puts difference;
+
+product = first_number * second_number;
+puts product
+
+quotient = first_number / second_number;
+puts quotient
